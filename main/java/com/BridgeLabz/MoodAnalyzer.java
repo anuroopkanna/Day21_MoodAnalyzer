86dd800eb032125bd600937e69f3de0b70ae630c
anuroopkanna <anuroopindia16@gmail.com>
2023-04-04 13:22:15 +0530

    added:uc1

--- main/java/com/BridgeLabz/MoodAnalyzer.java
+++ main/java/com/BridgeLabz/MoodAnalyzer.java
@@ -1,4 +1,17 @@
 package com.BridgeLabz;
 
 public class MoodAnalyzer {
+    String message;
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        super();
+        this.message = message;
+    }
+    public String analyseability(){
+        if (message.contains("sad"))
+            return "Sad";
+            return "Happy";
+        }
 }
--- test/java/com/Bridgelabz/MoodAnalyzerTest.java
+++ test/java/com/Bridgelabz/MoodAnalyzerTest.java
@@ -1,4 +1,25 @@
 package com.Bridgelabz;
 
+import com.BridgeLabz.MoodAnalyzer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MoodAnalyzerTest {
+    static MoodAnalyzer moodAnalyzer;
+    @Test
+    public void testAnalyseAbilityMethod_thenAssertionHappy() {
+        moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+        assertEquals("HAPPY", moodAnalyzer.analyseability());
+        System.out.println("UC1: Test Case For Happy Passed");
+    }
+
+    @Test
+    public void testAnalyseAbilityMethod_thenAssertionSad() {
+        moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        assertEquals("SAD", moodAnalyzer.analyseability());
+        System.out.println("UC1: Test Case For Sad Passed");
+
+    }
+
 }
